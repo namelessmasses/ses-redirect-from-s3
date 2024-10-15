35e5963eaa6494f0bf617f46a3ae3a116987a4a6
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2024-10-14 19:15:01 -0700

    Refactor error handling in lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -80,6 +80,5 @@ def lambda_handler(event, context):
         )
         print(response)
     except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        print('Exception: ' + e)
         raise e
