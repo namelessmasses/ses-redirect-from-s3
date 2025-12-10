33c6d830381ceae5a7a2b50fcc25cce2f4088170
Michael Kiwi Ngarimu <24663124+namelessmasses@users.noreply.github.com>
2025-12-10 14:49:21 -0800

    Add comments to Publish.ps1 for clarity on versioning and publishing steps

--- Publish.ps1
+++ Publish.ps1
@@ -1,3 +1,5 @@
 7z u -spf2 -r -tzip code.zip src/lambda_function.py
+# Create a new version for rollback
 aws lambda publish-version --function-name ses-redirect-from-s3 --no-cli-pager
+# Publish new code
 aws lambda update-function-code --function-name ses-redirect-from-s3 --zip-file fileb://./code.zip --no-cli-pager
